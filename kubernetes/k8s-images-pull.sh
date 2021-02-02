06172b8672684d080b88f3100cd9dabd562682e4
xcbeyond <xcbeyond157@163.com>
2021-02-02 19:51:53 +0800

    update

--- kubernetes/k8s-images-pull.sh
+++ kubernetes/k8s-images-pull.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-kubernetes-version = v1.20.0
+kubernetes_version="v1.20.0"
 
 # 下载需要的镜像(docker hub)
-kubeadm config images list --kubernetes-version=${kubernetes-version} |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#xcbeyond#g' |sh -x
+kubeadm config images list --kubernetes-version=${kubernetes_version} |sed -e 's/^/docker pull /g' -e 's#k8s.gcr.io#xcbeyond#g' |sh -x
 
 # 重命名镜像
 docker images |grep xcbeyond |awk '{print "docker tag ",$1":"$2,$1":"$2}' |sed -e 's#xcbeyond#k8s.gcr.io#2' |sh -x
